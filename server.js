f84305bcc80a56c109664ec1c896e7a2dc2d88c9
DonovanGallaway <91157673+DonovanGallaway@users.noreply.github.com>
2021-11-12 11:22:42 -0800

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ const express = require('express')
 const morgan = require('morgan')
 const mongoose = require('mongoose') // This can be changed to './database/connection' if you make a connection.js file
 const cors = require('cors')
-const { application } = require('express')
 const app = express()
 const {PORT = 3000} = process.env
 
